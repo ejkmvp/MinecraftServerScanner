808ec6f3f16badde3381339ad2e48f9c0efb944d
ejkmvp@umsystem.edu <ejkmvp@umsystem.edu>
2026-01-18 14:12:42 -0600

    more work on script and added more of my thoughts to plan.txt

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import threading
 import time
 import logging
+import queue
 from scapy.all import send
 
 #TODO better setup the logger
 logging.basicConfig(level=logging.DEBUG)
 
-PACKET_SEND_INTERVAL = .1
-SCAN_INTERVAL = 60 * 30
+PACKET_SEND_INTERVAL = .1       # 100 milliseconds
+SCAN_INTERVAL = 60 * 10         # 10 minutes  
+INTERVALS_PER_DEAD_SCAN = 6     # 60 minutes
 
 send_lock = threading.Lock()
 data_lock = threading.Lock()
@@ -27,12 +29,57 @@ def sendPacket(packet):
 def logData(data):
     pass
 
+#buffer for new connection table entries
 connectionTableBuffer = []
+
+#buffer for new player information table entries
 playerInformationBuffer = []
+
+#buffer for new server information table entries
 serverInformationBuffer = []
+
+#Dictionary from IP Addresses to TTLs
+ipAddressTable = {}
+
+#Send queue - queue for the initial TCP packets to send out
+initialSendQueue = queue.Queue()
+sendQueue = queue.Queue()
+
+
+
+def senderThread():
+    global initialSendQueue
+    global sendQueue
+    while True:
+        message = None
+        if sendQueue.size() != 0:
+            message = sendQueue.pop(0)
+            logging.debug("Sending message from sendqueue")
+        elif initialSendQueue.size() != 0:
+            message = initialSendQueue.pop(0)
+            logging.debug("Sending message from initial send queue")
+        if message is not None:
+            sendPacket(message)
+
+def receiverThread():
+    
+
+
 
 #Main Loop
 lastScanTime = 0
 while True:
     if time.time() > lastScanTime + SCAN_INTERVAL:
-        logging.debug
+        logging.debug("SCAN Interval reached, starting a scan")
+        lastScanTime = time.time()
+
+        #for each ip address, if the TTL is 0, then add it. Then, set the TTL to 6
+        keyList = list(ipAddressTable.keys())
+        for key in keyList:
+            ipAddressTable[key] -= 1
+            if ipAddressTable[key] == 0:
+                ipAddressTable[key] = 6
+                logging.debug(f"Adding ip Address {key} to send queue")
+                initialSendQueue.put(createTCPPacket(key))
+
+
